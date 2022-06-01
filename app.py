a665595b870811fb262a257c15ed18cc0bfb8ca3
Satvik <90170287+risen54@users.noreply.github.com>
2022-06-01 13:34:44 +0530

    add error handling

--- app.py
+++ app.py
@@ -4,35 +4,44 @@ from os import system
 # import termcolors (for upcoming update)
 
 while True:
-    # getting and printing worldwide info
-    print("Loading...")
-    
-    r = requests.get("https://disease.sh/v3/covid-19/all")
-    r = r.text
-    info = json.loads(r)
-    
-    system('clear')
-    print(f"""
-    Global Info:
-    Total Cases: {info["cases"]}
-    Active Cases: {info["active"]}
-    Total Deaths: {info["deaths"]}
-    """)
+    try:
+        # getting and printing worldwide info
+        print("Loading...")
+        
+        r = requests.get("https://disease.sh/v3/covid-19/all")
+        r = r.text
+        info = json.loads(r)
+        
+        system('clear')
+        print(f"""
+        Global Info:
+        Total Cases: {info["cases"]}
+        Active Cases: {info["active"]}
+        Total Deaths: {info["deaths"]}
+        """)
 
-    # Contry-wise info
-    country = input("Enter the name of the country => ").lower()
-    print("Loading...")
-    
-    r = requests.get(f"https://disease.sh/v3/covid-19/countries/{country}")
-    r = r.text
-    info = json.loads(r)
-    
-    system('clear')
-    print(f"""
-    {country} info:
-    Total Cases: {info["cases"]}
-    Active Cases: {info["active"]}
-    Total Deaths: {info["deaths"]}
-    """)
-    input("Press Enter to continue")
+        # Contry-wise info
+        country = input("Enter the name of the country => ").lower()
+        print("Loading...")
+        
+        r = requests.get(f"https://disease.sh/v3/covid-19/countries/{country}")
+        r = r.text
+        info = json.loads(r)
+        
+        try:
+            system('clear')
+            print(f"""
+            {country} info:
+            Total Cases: {info["cases"]}
+            Active Cases: {info["active"]}
+            Total Deaths: {info["deaths"]}
+            """)
+        except KeyError:
+            print("The country doesn't exist! Check if there's a typo")
 
+        input("Press Enter to continue")
+    
+    except KeyboardInterrupt:
+        system('clear')
+        print("BYEE!!")
+        exit()
